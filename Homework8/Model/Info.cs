f18f3eebbb04a861c0568fe1c9090a420c8d6f6e
Konstantin Prokhorov <fandom170@gmail.com>
2020-02-06 19:44:01 +0200

    Changes after practice 06/02/2020

---
+++ Homework8/Model/Info.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8.Model
+{
+    public class Info
+    {
+        [JsonProperty(PropertyName = "shipments")]
+        public List<Shipment> Shipments { get; set; }
+    }
+}
---
+++ Homework8/PrintInvoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class PrintInvoice
+    {
+        private Invoice invoiceForPrint;
+        private Boolean printOrders = false;
+        public PrintInvoice(Invoice invoice, Boolean printOrders) 
+        {
+            this.invoiceForPrint = invoice;
+            this.printOrders = printOrders;
+        }
+
+        public void PrintInvoices() 
+        {
+            //foreach (Shipment shipment in invoiceForPrint.shipments)
+            //{
+                //PrintShipment
+                    //}
+        }
+        
+    
+
+        public void PrintShipment(Shipment shipment, Boolean printOrders)
+        {
+            Console.WriteLine("Shipment name = " + shipment.Name);
+            //...
+        }
+
+        public void PrintOrder(Order order)
+        {
+
+        }
+    }
+}
